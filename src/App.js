fa3f9647d34b409db479d92938e7651e7fef9fdc
Ben Kiboma <omayioben@gmail.com>
2022-07-08 00:02:37 +0300

    Add Book components to the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import React from 'react';
+import Calc from './pages/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NotMatch from './pages/NoMatch';
+import Navbar from './pages/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calc />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
+
