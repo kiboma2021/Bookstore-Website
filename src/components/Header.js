660485993f90e36701f029676bffd76b758bbf80
Ben Kiboma <omayioben@gmail.com>
2022-07-07 23:27:14 +0300

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Bookstore CMS</h1>
+      <ul>
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+      </ul>
+    </div>
+  )
+};
+export default Header;
