bb02f5a6af3274bcb78c44236193b0b4d5bee6c5
Ben Kiboma <omayioben@gmail.com>
2022-07-07 23:47:08 +0300

    Add Book List

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 const AddBook = () => {
   return (
-    <form className="form-container">
-      <input
-        type="text"
-        placeholder="Add book title"
-        name="title"
-        className="input-book"
-      />
-      <input
-        type="text"
-        placeholder="Add author"
-        name="author"
-        className="input-author"
-      />      
-    </form>
+    <div>
+      <h2>ADD NEW BOOK</h2>
+      <form className="form-container">
+        <input
+          type="text"
+          placeholder="Book title"
+          name="title"
+          className="input-book"
+        />
+        <input
+          type="text"
+          placeholder="Add author"
+          name="author"
+          className="input-author"
+        />      
+      </form>
+    </div>
   );
 };
 
---
+++ src/components/Books.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => () => {
+    //console.log('Cleaning up ...');
+  }, []);
+
+  const handleEditting = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+  const { completed, id, title } = props.todo;
+
+  const viewMode = {};
+  const editMode = {};
+
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditting} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.delToDoProps(id)}>
+          <FaTrash />
+        </button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className="text-input"
+        value={title}
+        onChange={(e) => {
+          props.setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+export default TodoItem;
+
