760bd1decc8c3cbb18799997f19e2c146e41e77b
Ben Kiboma <omayioben@gmail.com>
2022-07-08 00:12:42 +0300

    Add Book components to the App

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
 import React from 'react';
-import Calc from './pages/Calculator';
-import Home from './pages/Home';
-import Quote from './pages/Quote';
-import NotMatch from './pages/NoMatch';
-import Navbar from './pages/Navbar';
+import AddBook from './components/AddBook';
+import Books from './components/Books';
+import Header from './components/Header';
+import Categories from './components/Categories';
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calc />} />
-        <Route path="/quote" element={<Quote />} />
-        <Route path="*" element={<NotMatch />} />
+        <Route path="/" element={<Books />} />
+        <Route path="/category" element={<Categories />} />
       </Routes>
     </Router>
   );
